5b319aa0f4a6caed456bedb200c46ea59e3887ee
holdenc20 <league@WTS7.attlocal.net>
2018-06-10 10:22:17 -0700

    done
    done

--- src/_02_debug_me/Marker.java
+++ src/_02_debug_me/Marker.java
@@ -13,15 +13,15 @@ class Marker
     { 
         if (mark >= 85) 
             System.out.println("High Distinction"); 
-        if (mark >= 75) 
+        else if (mark >= 75) 
             System.out.println("Distinction"); 
-        if (mark >= 65) 
+        else if (mark >= 65) 
             System.out.println("Credit"); 
-        if (mark >= 50) 
+        else if (mark >= 50) 
             System.out.println("Pass"); 
-        if (mark >= 45) 
+        else if (mark >= 45) 
             System.out.println("Concessional Pass"); 
-        if (mark < 45) 
+        else if (mark < 45) 
             System.out.println("Fail"); 
     } 
 } 
